60c482cbbc33c8cf8990fce9c15b959dbe772f1b
abdenigara1 <monsterturtle002@gmail.com>
2025-07-03 14:11:27 +0700

    Pesan commit pertama

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -12,82 +12,131 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          title: Text("ini Homepage"),
-          centerTitle: true,
-          shape: const RoundedRectangleBorder(
-              borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(20),
-                  bottomRight: Radius.circular(20))),
-          actions: [
-            IconButton(
-              onPressed: () {},
-              icon: Icon(Icons.search),
-            ),
-            IconButton(
-                onPressed: () {}, icon: Icon(Icons.notification_important))
-          ],
-          backgroundColor: const Color.fromARGB(255, 0, 140, 255),
-          elevation: 50.0,
-          leading: IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.menu),
-            tooltip: "menu icon",
-          )),
-      backgroundColor: const Color.fromARGB(255, 255, 255, 255),
-      body: Column(
-        children: [
-          SizedBox(height: 10),
-          Text(
-            "LOREM IPSUM",
-            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        title: Text("Ini Homepage"),
+        centerTitle: true,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(15),
+            bottomRight: Radius.circular(15),
           ),
-          Container(
-            height: 300,
-            width: double.infinity,
-            margin: const EdgeInsets.all(10.0),
-            decoration: BoxDecoration(
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.search),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.notification_important),
+          ),
+        ],
+        backgroundColor: const Color.fromARGB(255, 0, 140, 255),
+        elevation: 50.0,
+        leading: IconButton(
+          onPressed: () {},
+          icon: const Icon(Icons.menu),
+          tooltip: "Menu icon",
+        ),
+      ),
+      backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+
+
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(height: 10),
+            const Text(
+              "LOREM IPSUM",
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            Container(
+              height: 200,
+              width: double.infinity,
+              margin: const EdgeInsets.all(20.0),
+              decoration: BoxDecoration(
                 color: Colors.grey.withOpacity(0.2),
-                borderRadius: BorderRadius.circular(20),
+                borderRadius: BorderRadius.circular(10),
                 boxShadow: [
                   BoxShadow(
-                      color: Colors.black.withOpacity(0.2),
-                      offset: Offset.zero,
-                      blurRadius: 10.0)
-                ]),
-            child: Text(
-              "welcome",
+                    color: Colors.black.withOpacity(0.2),
+                    offset: Offset.zero,
+                    blurRadius: 10.0,
+                  )
+                ],
+              ),
+              child: const Center(
+                child: Text("Welcome"),
+              ),
             ),
-          ),
-          Container(
+            Container(
+              height: 200,
+              width: 200,
+              padding: const EdgeInsets.all(20),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Image.asset('asset/images/GAMBAR.JPG'),
+              ),
+            ),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: const [
+                ElevatedButton(
+                  onPressed: null,
+                  child: Text("Add to cart"),
+                ),
+                Icon(Icons.shopping_cart),
+                SizedBox(width: 10),
+                Icon(Icons.favorite),
+              ],
+            ),
+            const SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  labelText: 'Nama',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            SizedBox(
             height: 200,
-            width: 200,
-            padding: EdgeInsets.all(20),
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(20),
-              child: Image.asset('asset/images/GAMBAR.JPG'),
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: [
+                for (var image in [
+                  'Capture.PNG',
+                  'Capture1.PNG',
+                  'Capture2.PNG',
+                  'Capture3.PNG',
+                  'GAMBAR.JPG',
+                ])
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(20),
+                      child: Image.asset(
+                        'asset/images/$image',
+                        width: 150, // Tambahkan ini agar geser terasa
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+              ],
             ),
           ),
-          SizedBox(
-            height: 20,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              ElevatedButton(
-                onPressed: () {},
-                child: Text("add to cart"),
-              ),
-              Icon(Icons.shopping_cart),
-              SizedBox(
-                width: 10,
-              ),
-              Icon(Icons.favorite),
-            ],
-          )
-        ],
+
+
+          ],
+        ),
       ),
-      floatingActionButton:
-          FloatingActionButton(onPressed: () {}, child: Icon(Icons.add)),
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+      ),
     );
   }
 }
